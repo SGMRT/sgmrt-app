5542ad8130ac6b4b72c0e11f28474a36ccb37eea
seokkkkkk <tjr6613@seoultech.ac.kr>
2025-06-15 23:10:55 +0900

    feat: Locate Me button 추가

--- assets/svgs/svgs.tsx
+++ assets/svgs/svgs.tsx
@@ -1,5 +1,6 @@
 import StatsIcon from "./chart.svg";
+import LocateMe from "./focus.svg";
 import HomeIcon from "./map.svg";
 import ProfileIcon from "./user.svg";
 
-export { HomeIcon, ProfileIcon, StatsIcon };
+export { HomeIcon, LocateMe, ProfileIcon, StatsIcon };
--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,24 +1,57 @@
+import { LocateMe } from "@/assets/svgs/svgs";
 import {
     Camera,
+    Image,
+    Images,
+    LocationPuck,
     MapView,
     setTelemetryEnabled,
     StyleImport,
+    UserLocation,
+    Viewport,
 } from "@rnmapbox/maps";
-import { useEffect } from "react";
-import { View } from "react-native";
+import { useEffect, useState } from "react";
+import { TouchableOpacity, View } from "react-native";
 
 export default function Home() {
+    const [isFollowing, setIsFollowing] = useState(true);
     useEffect(() => {
         setTelemetryEnabled(false);
-    });
+    }, []);
+
+    const moveToUserLocation = () => {
+        setIsFollowing(true);
+    };
+
+    const onStatusChanged = (status: any) => {
+        if (status.to.kind === "idle") {
+            setIsFollowing(false);
+        }
+    };
+
     return (
         <View style={{ flex: 1 }}>
             <MapView
                 style={{ flex: 1 }}
                 scaleBarEnabled={false}
                 logoEnabled={false}
+                attributionPosition={{ bottom: 10, left: 10 }}
                 styleURL="mapbox://styles/sgmrt/cmbx0w1xy002701sod2z821zr"
             >
+                <Images>
+                    <Image name="puck">
+                        <View
+                            style={{
+                                borderColor: "white",
+                                borderWidth: 3,
+                                width: 23,
+                                height: 23,
+                                borderRadius: 100,
+                                backgroundColor: "#cfe900",
+                            }}
+                        />
+                    </Image>
+                </Images>
                 <StyleImport
                     id="basemap"
                     existing
@@ -34,10 +67,39 @@ export default function Home() {
                 />
                 <Camera
                     zoomLevel={16}
-                    centerCoordinate={[128.593444283, 35.865496405]}
                     animationDuration={0}
+                    followUserLocation={isFollowing}
+                    minZoomLevel={16}
+                    maxZoomLevel={16}
                 />
+                <Viewport onStatusChanged={onStatusChanged} />
+                <LocationPuck
+                    visible={true}
+                    topImage="puck"
+                    pulsing={{
+                        isEnabled: true,
+                        color: "#cfe900",
+                        radius: 22,
+                    }}
+                />
+                <UserLocation visible={false} />
             </MapView>
+            <TouchableOpacity
+                style={{
+                    position: "absolute",
+                    bottom: 20,
+                    right: 20,
+                    backgroundColor: "rgba(75, 75, 75, 0.8)",
+                    borderRadius: 100,
+                    width: 49.53,
+                    height: 49.53,
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+                onPress={moveToUserLocation}
+            >
+                <LocateMe />
+            </TouchableOpacity>
         </View>
     );
 }
