653e0f8d13a0c59fecfc236e738cda401fa4e255
seokkkkkk <tjr6613@seoultech.ac.kr>
2025-06-16 00:40:04 +0900

    feat: 메인화면 코스 선 출력 구현

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,20 +1,56 @@
 import { LocateMe } from "@/assets/svgs/svgs";
 import {
     Camera,
+    CircleLayer,
     Image,
     Images,
+    LineLayer,
     LocationPuck,
     MapView,
     setTelemetryEnabled,
+    ShapeSource,
     StyleImport,
     UserLocation,
     Viewport,
 } from "@rnmapbox/maps";
+
 import { useEffect, useState } from "react";
 import { TouchableOpacity, View } from "react-native";
 
+const circularCourse = Array.from({ length: 100 }, (_, i) => {
+    const pi = (2 * Math.PI * i) / 120;
+    const radius = 0.002;
+    const lon = 126.9502078182 + radius * Math.cos(pi);
+    const lat = 37.5439458182 + radius * Math.sin(pi);
+    return [lon, lat];
+});
+
 export default function Home() {
     const [isFollowing, setIsFollowing] = useState(true);
+
+    const styles = {
+        lineLayer: {
+            lineCap: "round",
+            lineJoin: "round",
+            lineWidth: 3,
+            lineEmissiveStrength: 1,
+            lineGradient: [
+                "interpolate",
+                ["linear"],
+                ["line-progress"], // 선 길이 비율 0~1
+                0,
+                "#ffffff",
+                1,
+                "#CFE900",
+            ],
+        },
+        startCircle: {
+            circleRadius: 6,
+            circleColor: "#ffffff",
+            circleEmissiveStrength: 1,
+        },
+    };
+
     useEffect(() => {
         setTelemetryEnabled(false);
     }, []);
@@ -35,7 +71,7 @@ export default function Home() {
                 style={{ flex: 1 }}
                 scaleBarEnabled={false}
                 logoEnabled={false}
-                attributionPosition={{ bottom: 10, left: 10 }}
+                attributionPosition={{ bottom: 20, left: 20 }}
                 styleURL="mapbox://styles/sgmrt/cmbx0w1xy002701sod2z821zr"
             >
                 <Images>
@@ -66,11 +102,11 @@ export default function Home() {
                     }}
                 />
                 <Camera
-                    zoomLevel={16}
+                    minZoomLevel={14}
+                    maxZoomLevel={18}
+                    followZoomLevel={16}
                     animationDuration={0}
                     followUserLocation={isFollowing}
-                    minZoomLevel={16}
-                    maxZoomLevel={16}
                 />
                 <Viewport onStatusChanged={onStatusChanged} />
                 <LocationPuck
@@ -83,6 +119,39 @@ export default function Home() {
                     }}
                 />
                 <UserLocation visible={false} />
+                <ShapeSource
+                    id="line-source"
+                    lineMetrics={1}
+                    shape={{
+                        type: "Feature",
+                        properties: {
+                            color: "#ffffff",
+                        },
+                        geometry: {
+                            type: "LineString",
+                            coordinates: circularCourse,
+                        },
+                    }}
+                >
+                    <LineLayer id="line-layer" style={styles.lineLayer} />
+                </ShapeSource>
+                {circularCourse.length > 0 && (
+                    <ShapeSource
+                        id="start-point-source"
+                        shape={{
+                            type: "Feature",
+                            geometry: {
+                                type: "Point",
+                                coordinates: circularCourse[0],
+                            },
+                        }}
+                    >
+                        <CircleLayer
+                            id="start-point-layer"
+                            style={styles.startCircle}
+                        />
+                    </ShapeSource>
+                )}
             </MapView>
             <TouchableOpacity
                 style={{
