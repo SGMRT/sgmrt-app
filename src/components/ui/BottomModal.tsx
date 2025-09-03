dad9fb7c1e0119f3e146e2024f9ac18102832e3e
seokkkkkk <tjr6613@seoultech.ac.kr>
2025-09-03 15:28:55 +0900

    feat: 바텀시트 모달에 백드롭 추가

--- src/components/ui/BottomModal.tsx
+++ src/components/ui/BottomModal.tsx
@@ -1,5 +1,10 @@
 import colors from "@/src/theme/colors";
-import { BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet";
+import {
+    BottomSheetBackdrop,
+    BottomSheetModal,
+    BottomSheetView,
+} from "@gorhom/bottom-sheet";
+import { useCallback } from "react";
 import { Dimensions, StyleProp, StyleSheet, ViewStyle } from "react-native";
 import { SharedValue } from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -14,6 +19,7 @@ interface BottomModalProps {
     index?: number;
     bottomInset?: number;
     onDismiss?: () => void;
+    backdrop?: boolean;
 }
 
 export default function BottomModal({
@@ -26,32 +32,48 @@ export default function BottomModal({
     index,
     bottomInset,
     onDismiss,
+    backdrop = true,
 }: BottomModalProps) {
     const { bottom } = useSafeAreaInsets();
+    const renderBackdrop = useCallback(
+        (props: any) => (
+            <BottomSheetBackdrop
+                {...props}
+                appearsOnIndex={0}
+                disappearsOnIndex={-1}
+                pressBehavior="close"
+                opacity={0.4}
+            />
+        ),
+        []
+    );
     return (
-        <BottomSheetModal
-            ref={bottomSheetRef}
-            bottomInset={bottomInset ?? bottom}
-            enablePanDownToClose={canClose}
-            backgroundStyle={styles.container}
-            handleStyle={[styles.handle, handleStyle]}
-            handleIndicatorStyle={styles.handleIndicator}
-            animatedPosition={heightVal ?? undefined}
-            snapPoints={snapPoints}
-            index={index}
-            onDismiss={onDismiss}
-            maxDynamicContentSize={Dimensions.get("window").height - 250}
-            keyboardBehavior="interactive"
-        >
-            <BottomSheetView
-                style={{
-                    minHeight: 280 - (bottomInset ?? bottom) - 35,
-                    justifyContent: "center",
-                }}
+        <>
+            <BottomSheetModal
+                ref={bottomSheetRef}
+                bottomInset={bottomInset ?? bottom}
+                enablePanDownToClose={canClose}
+                backgroundStyle={styles.container}
+                handleStyle={[styles.handle, handleStyle]}
+                handleIndicatorStyle={styles.handleIndicator}
+                animatedPosition={heightVal ?? undefined}
+                snapPoints={snapPoints}
+                index={index}
+                onDismiss={onDismiss}
+                maxDynamicContentSize={Dimensions.get("window").height - 250}
+                keyboardBehavior="interactive"
+                backdropComponent={backdrop ? renderBackdrop : undefined}
             >
-                {children}
-            </BottomSheetView>
-        </BottomSheetModal>
+                <BottomSheetView
+                    style={{
+                        minHeight: 280 - (bottomInset ?? bottom) - 35,
+                        justifyContent: "center",
+                    }}
+                >
+                    {children}
+                </BottomSheetView>
+            </BottomSheetModal>
+        </>
     );
 }
 
