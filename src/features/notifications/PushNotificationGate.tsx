1d6f3cff9a3b3a37a5359ae3656d4b02eb1bef0d
seokkkkkk <tjr6613@seoultech.ac.kr>
2025-09-05 09:55:13 +0900

    chore: PushNotificationGate 컴포넌트에서 디버깅 코드 제거

--- src/features/notifications/PushNotificationGate.tsx
+++ src/features/notifications/PushNotificationGate.tsx
@@ -1,20 +1,16 @@
 import { useEffect } from "react";
-import { Button, Text, View } from "react-native";
-import { sendPushNotification } from "./notifications";
+import { View } from "react-native";
 import { usePushNotifications } from "./usePushNotifications";
 
 export default function PushNotificationGate() {
     const { expoPushToken, notification } = usePushNotifications();
 
-    useEffect(() => {
-        console.log("expoPushToken", expoPushToken);
-        console.log("notification", notification);
-    }, [expoPushToken, notification]);
+    useEffect(() => {}, [expoPushToken, notification]);
 
     return (
-        <View style={{ display: "none" }}>
+        <View style={{ backgroundColor: "white", display: "none" }}>
             {/* 디버깅용 컴포넌트 */}
-            <View style={{ padding: 12 }}>
+            {/* <View style={{ padding: 12 }}>
                 <Text>Expo Push Token: {expoPushToken}</Text>
                 <Text>Title: {notification?.request.content.title ?? ""}</Text>
                 <Text>Body: {notification?.request.content.body ?? ""}</Text>
@@ -26,7 +22,7 @@ export default function PushNotificationGate() {
                     title="Test Push"
                     onPress={() => sendPushNotification(expoPushToken)}
                 />
-            </View>
+            </View> */}
         </View>
     );
 }
