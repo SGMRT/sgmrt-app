861520c5d4dba1daf571388778645ef98bec1eb5
seokkkkkk <tjr6613@seoultech.ac.kr>
2025-09-05 13:58:32 +0900

    feat: expo-live-activity 모듈 구조 변경 및 플랫폼별 모듈 로딩 방식 개선

---
+++ modules/expo-live-activity/index.ios.ts
@@ -0,0 +1,30 @@
+import { NativeModule, requireNativeModule } from "expo";
+import { ExpoLiveActivityModuleEvents, MessageType, RunType } from "./types";
+
+declare class ExpoLiveActivityModule extends NativeModule<ExpoLiveActivityModuleEvents> {
+    areActivitiesEnabled(): boolean;
+    isActivityInProgress(): boolean;
+    startActivity(
+        runType: RunType,
+        sessionId: string,
+        startedAt: string,
+        recentPace: number,
+        distanceMeters: number,
+        progress?: number,
+        message?: string,
+        messageType?: MessageType
+    ): Promise<boolean>;
+    updateActivity(
+        startedAt: string,
+        recentPace: number,
+        distanceMeters: number,
+        pausedAt?: string,
+        progress?: number,
+        message?: string,
+        messageType?: MessageType
+    ): void;
+    endActivity(): void;
+}
+
+const Native = requireNativeModule<ExpoLiveActivityModule>("ExpoLiveActivity");
+export default Native;
---
+++ modules/expo-live-activity/types.ts
@@ -0,0 +1,6 @@
+export type ExpoLiveActivityModuleEvents = {
+    onLiveActivityCancel: () => void;
+};
+
+export type RunType = "SOLO" | "GHOST" | "COURSE";
+export type MessageType = "INFO" | "WARNING" | "ERROR" | "SUCCESS";
