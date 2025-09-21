70ae8a6b4c989ae936349bd2f8202e6e4ea17b9c
seokkkkkk <tjr6613@seoultech.ac.kr>
2025-09-21 15:41:59 +0900

    feat: 워치 타겟 추가

--- targets/watch/expo-target.config.js
+++ targets/watch/expo-target.config.js
@@ -1,18 +1,12 @@
 /** @type {import('@bacons/apple-targets/app.plugin').ConfigFunction} */
 module.exports = (config) => ({
-    type: "widget",
-    name: "GhostRunner Widget",
+    type: "watch",
+    name: "GhostRunner Watch",
     icon: "../../assets/images/icon.png",
     colors: { $accent: "#e2ff00", $widgetBackground: "#111111" },
+    deploymentTarget: "9.4",
     entitlements: {
         "com.apple.security.application-groups":
             config.ios.entitlements["com.apple.security.application-groups"],
-    },
-    images: {
-        logo: {
-            "1x": "../../assets/images/widget/logo_1x.png",
-            "2x": "../../assets/images/widget/logo_2x.png",
-            "3x": "../../assets/images/widget/logo_3x.png",
-        },
     },
 });
