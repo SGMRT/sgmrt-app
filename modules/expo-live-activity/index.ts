2b8c20e5765e3e55fd9b8ee13f6f1502e0a92942
seokkkkkk <tjr6613@seoultech.ac.kr>
2025-09-23 13:32:00 +0900

    refactor: 플랫폼별 모듈 로딩 방식을 개선하여 가독성 향상

--- modules/expo-live-activity/index.ts
+++ modules/expo-live-activity/index.ts
@@ -1,11 +1,13 @@
 import { Platform } from "react-native";
+import androidModule from "./index.android";
+import iosModule from "./index.ios";
 
 // 플랫폼별로 적절한 모듈을 가져옴
 const expoLiveActivity = Platform.select({
-    ios: () => require("./index.ios").default,
-    android: () => require("./index.android").default,
-    default: () => require("./index.android").default, // fallback
-})();
+    ios: iosModule,
+    android: androidModule,
+    default: androidModule, // fallback
+});
 
 export default expoLiveActivity;
 export * from "./types";
