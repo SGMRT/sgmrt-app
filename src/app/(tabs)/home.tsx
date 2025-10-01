ef03e99dda5d5ad10dafe40eb015966a3e4ad32a
seokkkkkk <tjr6613@seoultech.ac.kr>
2025-10-01 11:34:41 +0900

    refactor: Home 컴포넌트에서 Healthkit 관련 코드 제거 및 useBootstrapApp으로 이동

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -10,40 +10,13 @@ import { SuccessToast } from "@/src/components/ui/toastConfig";
 import TopBlurView from "@/src/components/ui/TopBlurView";
 import { Typography } from "@/src/components/ui/Typography";
 import { useAuthStore } from "@/src/store/authState";
-import { devLog } from "@/src/utils/devLog";
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
-import {
-    AuthorizationRequestStatus,
-    useHealthkitAuthorization,
-} from "@kingstinct/react-native-healthkit";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useRef, useState } from "react";
 import { Confetti, ConfettiMethods } from "react-native-fast-confetti";
 
 export default function Home() {
     const [showListView, setShowListView] = useState(false);
-    const [authorizationStatus, requestAuthorization] =
-        useHealthkitAuthorization(
-            ["HKQuantityTypeIdentifierHeartRate"],
-            [
-                "HKQuantityTypeIdentifierDistanceWalkingRunning",
-                "HKQuantityTypeIdentifierActiveEnergyBurned",
-                "HKWorkoutTypeIdentifier",
-                "HKWorkoutRouteTypeIdentifier",
-            ]
-        );
-
-    useEffect(() => {
-        const checkAuthorization = async () => {
-            devLog("authorizationStatus", authorizationStatus);
-            if (
-                authorizationStatus === AuthorizationRequestStatus.shouldRequest
-            ) {
-                requestAuthorization();
-            }
-        };
-        checkAuthorization();
-    }, [authorizationStatus, requestAuthorization]);
 
     const bottomSheetRef = useRef<BottomSheetModal>(null);
     const confettiRef = useRef<ConfettiMethods | null>(null);
