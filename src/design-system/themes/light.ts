7a42d4fa75ad809c73b8473c48f4f620b955c94a
seokkkkkk <tjr6613@seoultech.ac.kr>
2026-01-25 22:22:23 -0800

    feat: themes/light.ts 추가 (placeholder)
    - Dark과 동일한 값 (추후 Light mode 적용 시 수정)

---
+++ src/design-system/themes/light.ts
@@ -0,0 +1,38 @@
+// Ghost Runner Design System - Light Theme (Placeholder)
+
+import { core, ghostLime, grey } from "../tokens/colors";
+import type { SemanticColors } from "./types";
+
+// TODO: Figma Light mode 값으로 업데이트
+export const lightTheme: SemanticColors = {
+    // UI Background & Surfaces
+    uiBackground: grey[110],
+    ui01: grey[100],
+    ui02: grey[90],
+    ui03: grey[80],
+    ui04: grey[70],
+    ui05: grey[60],
+    ui06: grey[50],
+    ui07: grey[40],
+    ui08: grey[30],
+    ui09: grey[20],
+    ui10: grey[10],
+
+    // UI Up (elevated surfaces)
+    uiUp: grey[100],
+    uiUp01: grey[90],
+    uiUp02: grey[80],
+    uiUp03: grey[70],
+
+    // Primary (Ghost Lime)
+    primary: core.primary,
+    primaryB: ghostLime[90],
+    primaryO: "rgba(226, 255, 0, 0.2)",
+
+    // Secondary (Ghost Red)
+    secondary: core.secondary,
+
+    // Tertiary
+    tertiary: grey[80],
+    tertiaryP: grey[90],
+};
